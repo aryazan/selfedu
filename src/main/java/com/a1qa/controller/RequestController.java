37af568ea71ab22664dff91de307df04c16a25bd
a.ryazantsev <artsiom.rasantsau@gmail.com>
2018-07-19 00:55:58 +0300

    working on charts

--- src/main/java/com/a1qa/controller/RequestController.java
+++ src/main/java/com/a1qa/controller/RequestController.java
@@ -1,11 +1,15 @@
 package com.a1qa.controller;
 
+import com.a1qa.model.Request;
 import com.a1qa.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Controller
@@ -27,4 +31,11 @@ public class RequestController {
         return "index";
     }
 
+    @GetMapping(path = "/getData/{urlPart}")
+    @ResponseBody
+    public List<Request> getRequestsByUrl(@PathVariable("urlPart") String urlPart) {
+        System.out.println("EST CONTACT: " + urlPart);
+
+        return requestService.getRequestByUrlLike(urlPart);
+    }
 }
